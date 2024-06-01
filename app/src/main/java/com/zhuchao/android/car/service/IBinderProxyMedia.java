5b9b5f4d4fa438876181ec56232a8748509fac1f
m <m@zhuchao.top>
2024-06-01 15:50:01 +0800

    refactor structure

--- app/src/main/java/com/zhuchao/android/car/service/IBinderProxyMedia.java
+++ app/src/main/java/com/zhuchao/android/car/service/IBinderProxyMedia.java
@@ -118,43 +118,43 @@ public class IBinderProxyMedia extends IMyMediaAidlInterface.Stub {
     public List<PMovie> getMediaList(int MsgID) {//获取媒体库媒体信息
         List<Movie> movies = new ArrayList<>();
         List<PMovie> pMovies = new ArrayList<>();
-        if (Cabinet.getPlayManager().getMediaManager() != null) {
+        if (Cabinet.getPlayManager().getMediaLibraryManager() != null) {
             switch (MsgID) {
                 case MessageEvent.MESSAGE_EVENT_LOCAL_VIDEO:
                     if (Cabinet.getPlayManager().getLocalMediaVideos().getCount() > 0)
                         movies = Cabinet.getPlayManager().getLocalMediaVideos().toList();
                     else
-                        movies = Cabinet.getPlayManager().getMediaManager().getLocalVideoSession().getVideoList().toList();
+                        movies = Cabinet.getPlayManager().getMediaLibraryManager().getLocalVideoSession().getVideoList().toList();
                     return transformToPMovie(movies);
                 case MessageEvent.MESSAGE_EVENT_USB_VIDEO:
                     if (Cabinet.getPlayManager().getLocalUSBMediaVideos().getCount() > 0)
                         movies = Cabinet.getPlayManager().getLocalUSBMediaVideos().toList();
                     else
-                        movies = Cabinet.getPlayManager().getMediaManager().getUSBVideoSession().getVideoList().toList();
+                        movies = Cabinet.getPlayManager().getMediaLibraryManager().getUSBVideoSession().getVideoList().toList();
                     return transformToPMovie(movies);//复制数据到AIDL空间
                 case MessageEvent.MESSAGE_EVENT_SD_VIDEO:
                     if (Cabinet.getPlayManager().getLocalSDMediaVideos().getCount() > 0)
                         movies = Cabinet.getPlayManager().getLocalSDMediaVideos().toList();
                     else
-                        movies = Cabinet.getPlayManager().getMediaManager().getSDVideoSession().getVideoList().toList();
+                        movies = Cabinet.getPlayManager().getMediaLibraryManager().getSDVideoSession().getVideoList().toList();
                     return transformToPMovie(movies);
                 case MessageEvent.MESSAGE_EVENT_LOCAL_AUDIO:
                     if (Cabinet.getPlayManager().getLocalMediaAudios().getCount() > 0)
                         movies = Cabinet.getPlayManager().getLocalMediaAudios().toList();
                     else
-                        movies = Cabinet.getPlayManager().getMediaManager().getLocalAudioSession().getVideoList().toList();
+                        movies = Cabinet.getPlayManager().getMediaLibraryManager().getLocalAudioSession().getVideoList().toList();
                     return transformToPMovie(movies);
                 case MessageEvent.MESSAGE_EVENT_USB_AUDIO:
                     if (Cabinet.getPlayManager().getLocalUSBMediaAudios().getCount() > 0)
                         movies = Cabinet.getPlayManager().getLocalUSBMediaAudios().toList();
                     else
-                        movies = Cabinet.getPlayManager().getMediaManager().getUSBAudioSession().getVideoList().toList();
+                        movies = Cabinet.getPlayManager().getMediaLibraryManager().getUSBAudioSession().getVideoList().toList();
                     return transformToPMovie(movies);
                 case MessageEvent.MESSAGE_EVENT_SD_AUDIO:
                     if (Cabinet.getPlayManager().getLocalSDMediaAudios().getCount() > 0)
                         movies = Cabinet.getPlayManager().getLocalSDMediaAudios().toList();
                     else
-                        movies = Cabinet.getPlayManager().getMediaManager().getSDAudioSession().getVideoList().toList();
+                        movies = Cabinet.getPlayManager().getMediaLibraryManager().getSDAudioSession().getVideoList().toList();
                     return transformToPMovie(movies);
             }
         }
